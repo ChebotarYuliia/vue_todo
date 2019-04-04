3a92364bae33490bc88102af939a6b7d9a3534cf
Julianna609 <chebotar609@gmail.com>
2019-04-04 23:09:45 +0300

    created todo-list components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{ todos.filter( todo => todo.done === true ).length }}</p>
+    <p>Pending Tasks: {{ todos.filter( todo => todo.done === false ).length }}</p>
+    <todo v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo" v-on:delete-todo="deleteTodo"></todo>
+  </div>
+</template>
+  
+
+<script type = "text/javascript" >
+import Todo from "./Todo.vue";
+export default {
+  props: ["todos"],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo(todo) {
+      const item = this.todos.indexOf(todo);
+      this.todos.splice(item, 1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
